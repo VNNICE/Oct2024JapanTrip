f59073997a77ce30d3966d5649857b8cbd371596
VNNICE <165227660+VNNICE@users.noreply.github.com>
2024-10-10 16:02:32 +0900

    test

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ window.onload = function()
     const sTimes = document.querySelectorAll('.t-sTime');
     const fTimes = document.querySelectorAll('.t-fTime');
     const summaries = document.querySelectorAll('.t-summary');
+    const costs = document.querySelectorAll('.t-cost');
     
     let placeWidth = 0;
     let sTimeWidth = 0;
     let fTimeWidth = 0;
     let summariesWidth = 0;
+    let costsWidth = 0;
     
     //Place
     places.forEach(e => {
@@ -45,5 +47,13 @@ window.onload = function()
         } 
     });
     summaries.forEach(e=>e.style.width = summariesWidth + 'px');
+    ///
+    //
+    costs.forEach(e => {
+        if (costsWidth < e.scrollWidth){
+            costsWidth = e.scrollWidth;
+        } 
+    });
+    costs.forEach(e=>e.style.width = costsWidth + 'px');
     ///
 };
