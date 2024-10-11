aecef1db4f590da36868a06a2f621f661fec0d9a
VNNICE <165227660+VNNICE@users.noreply.github.com>
2024-10-11 12:02:37 +0900

    fix

--- script.js
+++ script.js
@@ -27,12 +27,12 @@ window.onload = function()
 function RealTimeClock(){
     const date = new Date();
     const year = String(date.getFullYear());
-    const month = String(date.getMonth()).padStart(2, '0');
-    const day = String(date.getDay()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
     const hour = String(date.getHours()).padStart(2, '0');
     const minute = String(date.getMinutes()).padStart(2, '0');
     const second = String(date.getSeconds()).padStart(2, '0');
-    today.innerText = `${year}년 ${month}월 ${day}일 ${hour}:${minute}:${second}`
+    today.innerText = `${year}년 ${month}월 ${day}일 ${hour}:${minute}:${second}`;
 }
 
 function TotalCostCalculation(tables){
